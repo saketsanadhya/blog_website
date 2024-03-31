7883d379c31d21954bb962b033228a0d5446abed
saketsanadhya <saketsanadhya2003@gmail.com>
2024-03-31 18:42:14 +0530

    server setup

--- frontend/src/pages/UserAuthForm.page.jsx
+++ frontend/src/pages/UserAuthForm.page.jsx
@@ -1,70 +1,99 @@
-import AnimationWrapper from '../common/page-animation';
-import InputBox from '../components/InputBox';
-import googleIcon from "../imgs/google.png"
-import { Link } from 'react-router-dom';
-function UserAuthForm({type}) {
-  
+import { useRef } from "react";
+import AnimationWrapper from "../common/page-animation";
+import InputBox from "../components/InputBox";
+import googleIcon from "../imgs/google.png";
+import { Link } from "react-router-dom";
+import {Toaster,toast} from 'react-hot-toast'
+function UserAuthForm({ type }) {
+  const authForm = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let form = new FormData(authForm.current);
+    let formData = {};
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+    let { fullname, email, password } = formData;
+
+    if(fullname){
+    if (fullname.length < 3) {
+      return toast.error("full name must be at least 3 letter long");
+    }
+  }
+    if (!email.length) {
+      return toast.error("Enter email");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is invalid");
+    }
+    if (!passwordRegex.test(password)) {
+      toast.error("Password should be 6 to 20 characters long with 1 numeric,1 lowercase and 1 uppercase");
+    }
+
+  };
   return (
     <AnimationWrapper keyValue={type}>
-    <section className="h-cover flex items-center justify-center">
-      <form className='w-[80%] max-w-[400px]'>
-        <h1 className='text-4xl font-gelasio capitalize text-center mb-24'>
-          {type=="sign-in" ? "Welcome back":"Join Us Today"}
-        </h1>
-        { 
-          type!="sign-in"?<InputBox 
-            name="fullname"
-            type="text"
-            placeholder="Full Name"
-            icon="fi-rr-user"
-          /> : ""
-        }
-        <InputBox 
+      <section className="h-cover flex items-center justify-center">
+        <Toaster/>
+        <form ref={authForm} className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome back" : "Join Us Today"}
+          </h1>
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
+          <InputBox
             name="email"
             type="text"
             placeholder="Email"
             icon="fi-rr-envelope"
           />
-          <InputBox 
+          <InputBox
             name="password"
             type="password"
             placeholder="password"
             icon="fi-rr-key"
           />
-          <button className='btn-dark center mt-14'>
-            {type.replace("-"," ")}
+          <button className="btn-dark center mt-14" onClick={handleSubmit}>
+            {type.replace("-", " ")}
           </button>
-          <div className='relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold'>
-            <hr className='w-1/2 border-black'/>
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
             <p>OR</p>
-            <hr className='w-1/2 border-black'/>
-      </div>
-      <button className='btn-dark flex items-center justify-center gap-4 w-[90%] center'>
-        <img src={googleIcon} className='w-5' />
-        continue with google
-      </button>
-      {
-        type=="sign-in"?
-        <p className='mt-6 text-dark-grey text-xl text-center'>
-          Dont't have an account ?
-          <Link to='/signup' className='underline text-black text-xl ml-1'>
-            Join Us today
-          </Link>
-        </p>
-        :
-        <p className='mt-6 text-dark-grey text-xl text-center'>Already a Member ?
-          <Link to='/signin' className='underline text-black text-xl ml-1'>
-            Sign in here
-          </Link>
-        </p>
-      }
-      </form>
-      
-    </section>
+            <hr className="w-1/2 border-black" />
+          </div>
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" />
+            continue with google
+          </button>
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Dont't have an account ?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join Us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a Member ?
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Sign in here
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
     </AnimationWrapper>
   );
 }
 
 export default UserAuthForm;
-
-
